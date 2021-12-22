6804ae34694db9e566fd8fb35b8e844b29afe1b9
Diego Ruiz Fernández <diegorufe@gmail.com>
2021-12-22 05:37:02 +0100

    Añdida función para crear tabla de migraciones

--- service/mysqlservice.go
+++ service/mysqlservice.go
@@ -32,4 +32,32 @@ func ConnectMysqlDatabase(cacheProcess beans.CacheProcess, configuration beans.C
 
 	// Nos quedamos con la conexión de la base de datos
 	cacheProcess.DBSql = db
+
+	// Creamos versionado de la tabla
+	createVersionTable(cacheProcess, configuration)
+}
+
+// createVersionTable Método para crear el versioando de la tabla
+//
+// @parameters cacheProcess cache donde guardar ciertos datos del procesado
+//
+// @parameter configuration configuración que tiene todos los parámetros de configuración y comandos a procesar
+//
+// @returns --
+func createVersionTable(cacheProcess beans.CacheProcess, configuration beans.Configuration) {
+
+	query := "CREATE TABLE IF NOT EXISTS `rfchecbd_migrations` (" +
+		"	`id` int(11) NOT NULL auto_increment,   " +
+		"	`version` int(11) NOT NULL COMMENT 'Versión del módulo',     " +
+		"	`module` varchar(250)  NOT NULL COMMENT 'Nombre del módulo' ,  " +
+		"	`execDate` DATETIME  NOT NULL COMMENT 'Fecha de última ejecución del módulo' , " +
+		"	UNIQUE KEY `module_UNIQUE` (`module`),  " +
+		"	 PRIMARY KEY  (`id`)  " +
+		"  );"
+
+	_, err := cacheProcess.DBSql.Exec(query)
+
+	if err != nil {
+		log.Panicf("Se ha produdio un error en la creación de la tabla de versionado de la migración. %s", err)
+	}
 }
